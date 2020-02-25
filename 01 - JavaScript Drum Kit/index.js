5e65588894c117306c2dbcd2f4c1ef34fbb73886
Isaac Tait <isaac@mountaintopcoding.com>
2020-02-24 21:06:07 -0800

    debugging

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,19 +1,23 @@
+function removeTransition(event) {
+    if(event.propertyName !== 'transform') return;
+    //console.log(event.propertyName);
+    // this.classList.remove('playing');
+    event.target.classList.remove('playing'); //This line was in the finished.html but not in the course
+};
+
 function playSound(event) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
     if(!audio) return; // stop the function from running all together
+    
+    key.classList.add('playing')
     audio.currentTime = 0;
     audio.play();
-    key.classList.add('playing')
     //console.log(key);
     //console.log(audio);
     //console.log(event.keyCode);
 };
-function removeTransition(event) {
-    if(event.propertyName !== 'transform') return;
-    //console.log(event.propertyName);
-    this.classList.remove('playing');
-};
+
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
