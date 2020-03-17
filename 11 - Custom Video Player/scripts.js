b969525386a1f4f953d36aebce9caa3b2cf09f6d
Isaac Tait <isaac@mountaintopcoding.com>
2020-03-16 22:21:42 -0700

    lesson eleven (11) 2of2

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -29,14 +29,36 @@ function skip() {
 };
 
 function handleRangeUpdates() {
-    console.log(this.value)
+    video[this.name] = this.value
+};
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`
+};
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
 };
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 
+progress.addEventListener('click', scrub);
+
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdates));
+
+let mousedown = false;
+    progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+/* progress.addEventListener('mousemove', () => {
+    if(mousedown) {
+        scrub()
+    } */
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
